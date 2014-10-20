5adc6f3aab16e915b6e9a19edfe9fd14e114963e
siemiatj <kuba@saucelabs.com>
2014-10-20 17:07:52 +0200

    - fix working for multiple matches in one file

--- tasks/userev.js
+++ tasks/userev.js
@@ -66,33 +66,37 @@ module.exports = function (grunt) {
 
           for (var label in options.patterns) {
             var pattern = options.patterns[label];
-            var match = pattern.exec(content);
-            if (match) {
-              grunt.log.debug('Matching ' + [filepath, pattern, JSON.stringify(match)].join(': '));
-              replacement = match[0];
-              lastLink = match[1] || match[0];
-              baseLink = options.hash ? replaceFirstGroup(lastLink, options.hash, '') : lastLink;
-              for (var assetpath in versioned) {
-                if (endsWith(assetpath, baseLink)) {
-                  if (!updated) {
-                    grunt.log.writeln('Updating ' + filepath.cyan +
-                      (file.dest ? ' -> ' + file.dest.cyan : '.'));
-                  }
-                  hashLink = versioned[assetpath].slice(assetpath.length - baseLink.length);
-                  if (lastLink !== hashLink) {
-                    grunt.log.writeln('Linking ' + label + ': ' + lastLink +
-                      (baseLink !== lastLink ? ' -> ' + baseLink : '') + ' -> ' + hashLink.green);
-                    replacement = replacement.replace(lastLink, hashLink);
-                    content = content.replace(pattern, replacement);
-                    updated = true;
+            var matches = content.match(pattern);
+            var match;
+            if (matches) {
+              grunt.log.writeln('Matching ' + [filepath, pattern, JSON.stringify(matches)].join(': '));
+              for (var i=0; i<matches.length; i+=1){
+                match = [matches[i]];
+                replacement = match[0];
+                lastLink = match[1] || match[0];
+                baseLink = options.hash ? replaceFirstGroup(lastLink, options.hash, '') : lastLink;
+                for (var assetpath in versioned) {
+                  if (endsWith(assetpath, baseLink)) {
+                    if (!updated) {
+                      grunt.log.writeln('Updating ' + filepath.cyan +
+                        (file.dest ? ' -> ' + file.dest.cyan : '.'));
+                    }
+                    hashLink = versioned[assetpath].slice(assetpath.length - baseLink.length);
+                    if (lastLink !== hashLink) {
+                      grunt.log.writeln('Linking ' + label + ': ' + lastLink +
+                        (baseLink !== lastLink ? ' -> ' + baseLink : '') + ' -> ' + hashLink.green);
+                      replacement = replacement.replace(lastLink, hashLink);
+                      content = content.replace(pattern, replacement);
+                      updated = true;
+                    } else {
+                      grunt.log.writeln('Already linked ' + label + ': ' +
+                        baseLink + ' -> ' + hashLink.green);
+                    }
+                    break;
                   } else {
-                    grunt.log.writeln('Already linked ' + label + ': ' +
-                      baseLink + ' -> ' + hashLink.green);
+                    grunt.log.debug('No match: ' + lastLink +
+                      (baseLink !== lastLink ? ' -> ' + baseLink : '') + ' <> ' + assetpath);
                   }
-                  break;
-                } else {
-                  grunt.log.debug('No match: ' + lastLink +
-                    (baseLink !== lastLink ? ' -> ' + baseLink : '') + ' <> ' + assetpath);
                 }
               }
             } else {
